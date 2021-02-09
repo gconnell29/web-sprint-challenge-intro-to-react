221ed7aa5d7b271470a747977362bbcb76138191
gconnell29 <gconnelljr29@gmail.com>
2021-02-08 19:29:23 -0500

    successfully access API data using axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Character from './components/Character';
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characters, setCharacters] = useState();
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(response => {
+      let people = response.data;
+      console.log(`API response:`, people);
+    })
+    .catch(error => {
+      console.log('see error', error);
+    })
+  }, [])
 
   return (
     <div className="App">
